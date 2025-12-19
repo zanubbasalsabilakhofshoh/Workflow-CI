ae8f50f88e361f21e9b5e5ed8024ea481b501feb
zanubba <zanubba.salsabila.khofshoh-2022@fst.unair.ac.id>
2025-12-19 11:29:01 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,48 +1,29 @@
-import os
-import shutil
-import pandas as pd
 import mlflow
-import mlflow.sklearn
-
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+import pickle
 
-def main():
-    mlflow.set_tracking_uri("file:./mlruns")
-    mlflow.set_experiment("ci-training")
+df = pd.read_csv('../dataset_preprocessed/heart_disease_preprocessing.csv')
 
-    DATA_PATH = "../MLProject/dataset_preprocessed/heart_disease_preprocessing.csv"
+X = df.drop('target', axis=1)
+y = df['target']
 
-    df = pd.read_csv(DATA_PATH)
-    X = df.drop("target", axis=1)
-    y = df["target"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42, stratify=y
-    )
+model = RandomForestClassifier(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
 
-    with mlflow.start_run(run_name="rf_ci_run"):
-        mlflow.sklearn.autolog()
+y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
 
-        model = RandomForestClassifier(n_estimators=100, random_state=42)
-        model.fit(X_train, y_train)
+mlflow.log_param("n_estimators", 100)
+mlflow.log_metric("accuracy", accuracy)
 
-        y_pred = model.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
-        mlflow.log_metric("accuracy_manual", acc)
+with open('outputs/model.pkl', 'wb') as f:
+    pickle.dump(model, f)
 
-        mlflow.sklearn.log_model(model, "model")
+mlflow.log_artifact('outputs/model.pkl')
 
-        run_id = mlflow.active_run().info.run_id
-
-    os.makedirs("outputs", exist_ok=True)
-
-    run_artifacts = os.path.join("mlruns", "0", run_id, "artifacts")
-    if os.path.exists(run_artifacts):
-        shutil.make_archive("outputs/mlflow_artifacts", "zip", run_artifacts)
-
-    print("Training selesai (CI). Run ID:", run_id)
-
-if __name__ == "__main__":
-    main()
+print(f"Accuracy: {accuracy}")
